1b6025fbfef7e9d6ddc2b5d1b1ba59937e944cb5
Javey <jiawei23716@sina.com>
2014-12-22 21:48:41 +0800

    javey

---
+++ README.md
@@ -0,0 +1,13 @@
+# gulp-nocache
+
+重命名所有静态资源，并替换相应的引用路径，用于解决web上线后浏览器缓存静态资源的问题。[Web-NoCache](https://github.com/Javey/nocache)
+
+## Install
+
+```
+npm install gulp-nocache
+```
+
+## Usage
+
+`gulpfile.js`示例
---
+++ src/index.js
@@ -0,0 +1,44 @@
+/**
+ * @fileoverview
+ * @author javey
+ * @date 14-12-22
+ */
+
+var through = require('through2'),
+    gutil = require('gulp-util'),
+    path = require('path'),
+    Processor = require('web-nocache').Processor;
+
+const PLUGIN_NAME = 'gulp-nocache';
+
+map = {};
+
+function nocache(options) {
+    options = options || {
+        type: 'media',
+        dest: '[path][name].[ext]',
+        sourceContext: '',
+        outputContext: ''
+    };
+    return through.obj(function(file, enc, cb) {
+        if (file.isStream()) {
+            this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streams are not supported'));
+            return cb();
+        }
+
+        var contents = file.contents.toString(),
+            processor = Processor.getInstance(options.type),
+            outputFile = path.resolve(processor._getFilename(options.dest, file.path, options.sourceContext, contents));
+        processor.setMap(map);
+        contents = processor.process(contents, file.path, outputFile, options);
+        file.contents = new Buffer(contents);
+        map[file.path] = outputFile;
+        file.path = outputFile;
+
+        this.push(file);
+
+        cb();
+    });
+}
+
+module.exports = nocache;
