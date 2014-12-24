cd00e84790994f2f486fe6d0fb22c327e7cbc6a2
Javey <jiawei23716@sina.com>
2014-12-24 17:11:19 +0800

    处理文本后，再计算hash

--- src/index.js
+++ src/index.js
@@ -28,11 +28,15 @@ function nocache(options) {
 
         var contents = options.type === 'media' ? file.contents : file.contents.toString(),
             processor = Processor.getInstance(options.type),
-            outputFile = path.resolve(processor._getFilename(options.dest, file.path, options.sourceContext, contents));
+        // 刚开始，仅用于计算路径，不需要hash值，待处理文件后在计算hash
+            outputFile = path.resolve(processor._getFilename(options.dest, file.path, options.sourceContext));
 
         processor.setMap(map);
         contents = processor.process(contents, file.path, outputFile, options);
         file.contents = options.type === 'media' ? contents : new Buffer(contents);
+
+        // 经过processor.process处理后，计算hash值
+        outputFile = path.resolve(processor._getFilename(options.dest, file.path, options.sourceContext, contents));
 
         map[file.path] = outputFile;
 
